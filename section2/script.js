927fe7ca30b6a75112fbcfaea2b638597e95a99f
zakzakst <e1433104@gmail.com>
2023-02-08 09:44:29 +0900

    section2

--- section2/script.js
+++ section2/script.js
@@ -15,3 +15,26 @@
     this.status.html(this.options.message.replace(/%count%/, remaining));
   };
 })();
+
+(() => {
+  function CheckboxCollapser(checkbox, toggleElement) {
+    this.checkbox = checkbox;
+    this.toggleElement = toggleElement;
+    this.check();
+    this.checkbox.on('click', (e) => {
+      this.onCheckboxClick(e);
+    });
+  }
+
+  CheckboxCollapser.prototype.onCheckboxClick = function(e) {
+    this.check();
+  }
+
+  CheckboxCollapser.prototype.check = function() {
+    if (this.checkbox.prop('checked')) {
+      this.toggleElement.addClass('hidden');
+    } else {
+      this.toggleElement.removeClass('hidden');
+    }
+  }
+})();
